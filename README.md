6ce065de48c01fe642f56c00087b3686a5588768
Ivan Istomin <ivan@istomin.im>
2018-07-27 12:26:11 +0300

    Something

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ Solving problems from their series articles of [Algorithmic trading 101](https:/
 
 *   [x] **Problem 00** — _The withdrawal of a sample_
 *   [x] **Problem 01** — _Write a new MA, AR, and/or ARMA strategy using The Ocean X terminology/API tools_
-    *   [ ] **Bonus:** — _Backtest against publicly available data_
+    *   [ ] **Bonus** — _Backtest against publicly available data_
 *   [x] **Problem 02** — _Create your own mean reverting model using The Ocean X API_
-    *   [ ] **Bonus:** — _Backtest on data available at [CoinAPI.io](https://www.coinapi.io/)_
+    *   [ ] **Bonus** — _Backtest on data available at [CoinAPI.io](https://www.coinapi.io/)_
 *   [ ] **Problem 03** — _Create an arbitrage model between The Ocean X and Binance_
-    *   [ ] **Bonus:** — _Using Binance data, create a correlation matrix between pairs of tokens to find ‘equilibrium’ levels_
+    *   [ ] **Bonus** — _Using Binance data, create a correlation matrix between pairs of tokens to find ‘equilibrium’ levels_
--- src/problem_01.js
+++ src/problem_01.js
@@ -2,20 +2,19 @@
  * Code written by Ivan Istomin, 2018
  */
 
-import createOcean from 'the-ocean-x';
-import Web3 from 'web3';
-import * as R from 'ramda';
-import dotenv from 'dotenv/config';
-import {getTime, subHours, format} from 'date-fns';
-import CoinAPI from 'coinapi-io';
+import createOcean from 'the-ocean-x'
+import Web3 from 'web3'
+import * as R from 'ramda'
+import {getTime, subHours, format} from 'date-fns'
+import CoinAPI from 'coinapi-io'
 
-let position = 'in';
-let coinapi = new CoinAPI(process.env.COIN_API);
+let position = 'in'
+let coinapi = new CoinAPI(process.env.COIN_API)
 
 const update = async() => {
     const provider = new Web3
         .providers
-        .HttpProvider(process.env.WEB3_URL);
+        .HttpProvider(process.env.WEB3_URL)
 
     let ocean = await createOcean({
         api: {
@@ -24,31 +23,40 @@ const update = async() => {
             baseURL: process.env.BASE_URL
         },
         web3Provider: provider
-    });
+    })
 
     // Get first one pair (ZRX/WETH)
     const pairs = await ocean
         .marketData
-        .tokenPairs();
-    const myPair = pairs[0];
+        .tokenPairs()
+    const myPair = pairs[0]
 
     // Get historical price data for the period of time that we are taking the
     // moving average over (last 5 hours)
-    const startTime = parseInt(getTime(subHours(new Date(), 5)) / 1000);
-    const endTime = parseInt(getTime(subHours(new Date(), 1)) / 1000);
-    const interval = 3600;
+    const startTime = parseInt(getTime(subHours(new Date(), 5)) / 1000)
+    const endTime = parseInt(getTime(subHours(new Date(), 1)) / 1000)
+    const interval = 3600
     const candlesticks = await ocean
         .marketData
-        .candlesticks({baseTokenAddress: myPair.baseToken.address, quoteTokenAddress: myPair.quoteToken.address, startTime, endTime, interval});
+        .candlesticks({
+            baseTokenAddress: myPair.baseToken.address,
+            quoteTokenAddress: myPair.quoteToken.address,
+            startTime,
+            endTime,
+            interval
+        })
 
     // Calculate the moving average for this moment in time.
-    const movingAverage = R.mean(R.pluck('close')(candlesticks));
+    const movingAverage = R.mean(R.pluck('close')(candlesticks))
 
     // Get the last trading price
     const ticker = await ocean
         .marketData
-        .ticker({baseTokenAddress: myPair.baseToken.address, quoteTokenAddress: myPair.quoteToken.address});
-    const last = ticker.last;
+        .ticker({
+            baseTokenAddress: myPair.baseToken.address,
+            quoteTokenAddress: myPair.quoteToken.address
+        })
+    const last = ticker.last
 
     // Compare the last price to the moving average and change the current position
     // if approriate.
@@ -57,42 +65,67 @@ const update = async() => {
         // Buy up as much as you can
         const quoteBalance = await ocean
             .wallet
-            .getTokenBalance({etherAddress: process.env.BOT_ADDRESS, tokenAddress: myPair.quoteToken.address});
+            .getTokenBalance({
+                etherAddress: process.env.BOT_ADDRESS,
+                tokenAddress: myPair.quoteToken.address
+            })
 
         // This is an approximation of the most we can buy
         const baseAmountToBuy = quoteBalance
             .div(last)
-            .times(0.95); // multipliedBy()
+            .times(0.95) // multipliedBy()
 
-        console.log(await ocean.trade.newMarketOrder({baseTokenAddress: myPair.baseToken.address, quoteTokenAddress: myPair.quoteToken.address, side: 'buy', orderAmount: baseAmountToBuy, feeOption: 'feeInNative'}));
+        console.log(await ocean.trade.newMarketOrder({
+            baseTokenAddress: myPair.baseToken.address,
+            quoteTokenAddress: myPair.quoteToken.address,
+            side: 'buy',
+            orderAmount: baseAmountToBuy,
+            feeOption: 'feeInNative'
+        }))
 
-        position = 'in';
+        position = 'in'
     } else if (position === 'in') {
 
         // Sell off everything you can
         const baseBalance = await ocean
             .wallet
-            .getTokenBalance({etherAddress: process.env.BOT_ADDRESS, tokenAddress: myPair.baseToken.address});
-        console.log(await ocean.trade.newMarketOrder({baseTokenAddress: myPair.baseToken.address, quoteTokenAddress: myPair.quoteToken.address, side: 'sell', orderAmount: baseBalance, feeOption: 'feeInNative'}));
+            .getTokenBalance({
+                etherAddress: process.env.BOT_ADDRESS,
+                tokenAddress: myPair.baseToken.address
+            })
+        console.log(await ocean.trade.newMarketOrder({
+            baseTokenAddress: myPair.baseToken.address,
+            quoteTokenAddress: myPair.quoteToken.address,
+            side: 'sell',
+            orderAmount: baseBalance,
+            feeOption: 'feeInNative'
+        }))
 
-        position = 'out';
+        position = 'out'
     }
 
     /*
      * Run update function once per hour
      *
      * NB:
-     * From StackOverflow: setTimeout() ensures that timer events don't "stack" if they're
-     * left unprocessed. In some circumstances a whole load of setInterval events can arrive
-     * immediately after each other without any delay. (c) Alnitak
+     * From StackOverflow: setTimeout() ensures that timer
+     * events don't "stack" if they're left unprocessed.
+     * In some circumstances a whole load of setInterval
+     * events can arrive immediately after each other
+     * without any delay. (c) Alnitak
      */
-    setTimeout(update, 3600 * 1000);
+    setTimeout(update, 3600 * 1000)
 }
 
 const check_api = async() => {
-    let last_5_hrs = await coinapi.ohlcv_historic_data('POLONIEX_SPOT_ZRX_BTC', '1HRS', subHours(new Date(), 6), new Date());
-    console.log(last_5_hrs);
+    let last_5_hrs = await coinapi.ohlcv_historic_data(
+        'POLONIEX_SPOT_ZRX_BTC',
+        '1HRS',
+        subHours(new Date(), 6),
+        new Date()
+    )
+    console.log(last_5_hrs)
 }
 
-// update();
-check_api();
+// update()
+check_api()
